ef8e58af7f62005fc7778879d80e1d70191a1ddf
Pierre Hanna <realbrush@gmail.com>
2022-08-16 23:01:38 +0200

    m

--- Drawing Performance/Drawing Views/FreeDrawingImageViewDrawLayer.swift
+++ Drawing Performance/Drawing Views/FreeDrawingImageViewDrawLayer.swift
@@ -21,7 +21,7 @@ class FreeDrawingImageViewDrawLayer: UIView, Drawable {
     
     var brushImage: UIImage?
     override func didMoveToSuperview() {
-        self.brushImage = UIImage(named: "roundSoft1")?.withTintColor(UIColor.blue, renderingMode: .alwaysOriginal)
+        self.brushImage = UIImage(named: "brush1")?.withTintColor(UIColor.blue, renderingMode: .alwaysOriginal)
     }
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -33,15 +33,11 @@ class FreeDrawingImageViewDrawLayer: UIView, Drawable {
         let lastTouchPoint: CGPoint = line.last ?? .zero
         
         //let rect = calculateRectBetween(lastPoint: lastTouchPoint, newPoint: newTouchPoint)
-        
         //layer.setNeedsDisplay(rect)
+        
         layer.setNeedsDisplay()
     }
-    
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        flattenImage()
-//    }
-    
+
     override func draw(_ layer: CALayer, in ctx: CGContext) {
 
         guard
@@ -49,12 +45,6 @@ class FreeDrawingImageViewDrawLayer: UIView, Drawable {
             let size = self.brushImage?.size  else { return }
         
         for (index, point) in line.enumerated() {
-//            self.brushImage?.draw(at: point, blendMode: CGBlendMode.normal, alpha: 0.5)
-            
-            //debug
-            //print("point: \(point)")
-                
-            
 
             let rect = CGRect(x: point.x - size.width/2,
                               y: point.y - size.height/2,
@@ -62,42 +52,13 @@ class FreeDrawingImageViewDrawLayer: UIView, Drawable {
                               height: size.height)
             
 //            ctx.setFillColor(UIColor.blue.cgColor)
-            ctx.setAlpha(0.5)
+            ctx.setAlpha(0.2)
             ctx.draw(cg, in: rect)
 //            ctx.addEllipse(in: rect)
 //            ctx.strokePath()
-
         }
-
     }
-    
-//    func checkIfTooManyPoints() {
-//        let maxPoints = 25
-//        if line.count > maxPoints {
-//            updateFlattenedLayer()
-//            // we leave two points to ensure no gaps or sharp angles
-//            _ = line.removeFirst(maxPoints - 2)
-//        }
-//    }
-    
-//    func flattenImage() {
-//        updateFlattenedLayer()
-//        line.removeAll()
-//    }
-    
-//    func updateFlattenedLayer() {
-//        // 1
-//        guard let drawingLayer = drawingLayer,
-//            // 2
-//            let optionalDrawing = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(
-//                NSKeyedArchiver.archivedData(withRootObject: drawingLayer, requiringSecureCoding: false))
-//                as? CAShapeLayer,
-//            // 3
-//            let newDrawing = optionalDrawing else { return }
-//        // 4
-//        layer.addSublayer(newDrawing)
-//    }
-    
+
     func drawSpiralWithLink() {
         let link = CADisplayLink(target: self, selector: #selector(drawSpiral))
         link.add(to: .main, forMode: .default)
@@ -105,35 +66,7 @@ class FreeDrawingImageViewDrawLayer: UIView, Drawable {
     }
     
     @objc func drawSpiral() {
-//        if self.spiralPoints.isEmpty {
-////            emptyFlattenedLayers()
-//            drawingLayer?.removeFromSuperlayer()
-//            drawingLayer = nil
-//            line.removeAll()
-//            spiralPoints.removeAll()
-//            layer.setNeedsDisplay()
-//            self.createSpiral()
-//            self.flattenImage()
-//        } else {
-//            self.line.append(self.spiralPoints.removeFirst())
-//            self.layer.setNeedsDisplay()
-//            self.checkIfTooManyPoints()
-//        }
+
     }
-    
-//    func clear() {
-//        stopAutoDrawing()
-////        emptyFlattenedLayers()
-//        drawingLayer?.removeFromSuperlayer()
-//        drawingLayer = nil
-//        line.removeAll()
-//        spiralPoints.removeAll()
-//        layer.setNeedsDisplay()
-//    }
-    
-//    func emptyFlattenedLayers() {
-//        for case let layer as CAShapeLayer in sublayers {
-//            layer.removeFromSuperlayer()
-//        }
-//    }
+
 }
